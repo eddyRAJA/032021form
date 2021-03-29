178dde856728d2dbd4019e88b356f8d78d9988e7
eddyRAJA <79151853+eddyRAJA@users.noreply.github.com>
2021-03-30 00:58:57 +0200

    Update thanks.php

--- thanks.php
+++ thanks.php
@@ -1,6 +1,6 @@
 <?php
 
-echo htmlspecialchars( 'Merci ' . $_POST['first_name'] . ' ' .$_POST['user_name'] . ' de nous avoir contacté à propos de "' . $_POST['object'] . '". Un de nos conseillers vous contactera soit à l’adresse "' . $_POST['user_email'] . '" soit par téléphone, ' . $_POST['moment'] . ', au ' . $_POST['phone_number'] . ', dans les plus brefs délais pour traiter votre demande : ') . '<br>' ;
+echo htmlspecialchars( 'Merci ' . $_POST['first_name'] . ' ' .$_POST['user_name'] . ' de nous avoir contacté à propos "' . $_POST['object'] . '". Un de nos conseillers vous contactera soit à l’adresse "' . $_POST['user_email'] . '" soit par téléphone, au ' . $_POST['phone_number'] . ', dans les plus brefs délais pour traiter votre demande : ') . '<br>' ;
 
 echo htmlspecialchars($_POST['user_message'] );
 
