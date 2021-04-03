3142817c59f011d961fd9acd9c1cec5a3d0510a3
eddyRAJA <eddy-rajaonarivelo_student2021@wilder.school>
2021-04-03 11:39:40 +0200

    update for securing

--- form.php
+++ form.php
@@ -7,37 +7,37 @@
   <form  action="thanks.php"  method="POST">
     <div>
       <label  for="nom">Nom :</label>
-      <input  type="text"  id="nom"  name="user_name">
+      <input  type="text"  id="nom"  name="user_name">*
     </div><br>
     <div>
       <label  for="pnom">Prénom :</label>
-      <input  type="text"  id="pnom"  name="first_name">
+      <input  type="text"  id="pnom"  name="first_name">*
     </div><br>
     <div>
       <label  for="courriel">Courriel :</label>
-        <input  type="email"  id="courriel"  name="user_email">
+        <input  type="text"  id="courriel"  name="user_email">*
     </div><br>
     <div>
       <label  for="phone">Téléphone :</label>
-        <input  type="tel"  id="phone"  name="phone_number">
+        <input  type="tel"  id="phone"  name="phone_number">*
     </div><br>
     <div>
-        <label for="sujet">Objet :</label>
+    <label for="sujet">Objet :</label>
         <select id="sujet" name="object">
             <option value="la vente">la vente</option>
             <option value="du produit">du produit</option>
             <option value="du délai">du délai</option>
             <option value="du prix">du prix</option>
         </select><br>
-    </div>
+    </div><br>
     <div>
       <label  for="message">Message :</label>
-      <textarea  id="message"  name="user_message"></textarea>
-    </div><br>
-    
+      <textarea  id="message"  name="user_message"></textarea>*
+    </div><br>    
     <div  class="button">
       <button  type="submit">Envoyer votre message</button>
     </div>
+    <p><span class="error">* required field</span></p>
   </form>
 </fieldset>
 </body>
--- thanks.php
+++ thanks.php
@@ -1,8 +1,69 @@
 <?php
+// define variables and set to empty values
+$name = $firstname = $email = $phone = $object = $message = "";
 
-echo htmlspecialchars( 'Merci ' . $_POST['first_name'] . ' ' .$_POST['user_name'] . ' de nous avoir contacté à propos "' . $_POST['object'] . '". Un de nos conseillers vous contactera soit à l’adresse "' . $_POST['user_email'] . '" soit par téléphone, au ' . $_POST['phone_number'] . ', dans les plus brefs délais pour traiter votre demande : ') . '<br>' ;
+if ($_SERVER["REQUEST_METHOD"] ==  "POST") {
+    if (empty($_POST['user_name'])) {
+        $nameErr = 'Name is required, come back please';
+        echo $nameErr;
+        return;
+    } else {
+        $name = test_input($_POST["user_name"]);
+    }
+    if (empty($_POST['first_name'])) {
+        $nameErr = 'Firstname is required, come back please';
+        echo $nameErr;
+        return;
+    } else {
+        $firstname = test_input($_POST["first_name"]);
+    }
+    if (empty($_POST['user_email'])) {
+        $nameErr = 'Email is required, come back please';
+        echo $nameErr;
+        return;
+    } else {
+        $email = test_input($_POST["user_email"]);
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $emailErr = 'Invalid email format';
+            echo $emailErr;
+            return;
+        };
+    }
+    if (empty($_POST['phone_number'])) {
+        $nameErr = 'Phone number is required, come back please';
+        echo $nameErr;
+        return;
+    } else {
+        $phone = test_input($_POST["phone_number"]);
+    }
+    if (empty($_POST['object'])) {
+        $nameErr = 'Object is required, come back please';
+        echo $nameErr;
+        return;
+    } else {
+        $object = test_input($_POST["object"]);
+    }
+    if (empty($_POST['user_message'])) {
+        $nameErr = 'Message is required, come back please';
+        echo $nameErr;
+        return;
+    } else {
+        $message = test_input($_POST["user_message"]);
+    }
+};
 
-echo htmlspecialchars($_POST['user_message'] );
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
 
+
+/*var_dump($firstname);*/
+
+echo  'Merci ' . $firstname . ' ' . $name . ' de nous avoir contacté à propos "' . $object . '". Un de nos conseillers vous contactera soit à l’adresse "' . $email . '" soit par téléphone au ' . $phone . ', dans les plus brefs délais pour traiter votre demande : <br>' ;
+
+echo $message ;
 
 ?>
